7b0829018da642a4de24866900a4909f16682951
komaitaira <komaitaira@gmail.com>
2022-01-30 19:21:57 +0900

    Add Confirmコンポーネント

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Confirm } from "../components/pages/Confirm";
 import { Page404 } from "../components/pages/Page404";
 import { Period } from "../components/pages/Period";
 import { Theme } from "../components/pages/Theme";
@@ -9,6 +10,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<Theme />} />
         <Route path="period" element={<Period />} />
+        <Route path="confirm" element={<Confirm />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
