12aef825da399759fc4762bdb844dc58c983e153
komaitaira <komaitaira@gmail.com>
2022-02-21 00:45:55 +0900

    Fix DatePicker 初期値を設定

--- src/components/molecules/datepicker/PeriodDatePicker.tsx
+++ src/components/molecules/datepicker/PeriodDatePicker.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ja from "date-fns/locale/ja";
-import { selectedPeriodState } from "../../../store/selectState";
+import { getDateObj, selectedPeriodState } from "../../../store/selectState";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { Controller, useFormContext } from "react-hook-form";
@@ -14,22 +14,11 @@ export const PeriodDatePicker = () => {
   registerLocale("ja", ja);
   const { register, control } = useFormContext();
 
-  const getDate = (date: Date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const dayOfWeek = date.getDay();
-    return { year, month, day, dayOfWeek };
-  };
-
   const handleChange = (date: Date) => {
     console.log(date); // 選択した日時
     if (date) {
-      const d = getDate(date);
-      const dayOfWeekArray = ["日", "月", "火", "水", "木", "金", "土"];
-      const selectedDay = `${d.year}年${d.month}月${d.day}日(${
-        dayOfWeekArray[d.dayOfWeek]
-      })`;
+      const d = getDateObj(date);
+      const selectedDay = `${d.year}年${d.month}月${d.day}日(${d.dayOfWeek})`;
       setStartDate(new Date(`${d.year}-${d.month}-${d.day} 23:59:59`));
       setSelect({ selected: selectedDay });
     }
@@ -44,7 +33,7 @@ export const PeriodDatePicker = () => {
           {...register("period", {
             validate: () => {
               const now = new Date();
-              const d = getDate(now);
+              const d = getDateObj(now);
               return startDate >= new Date(`${d.year}-${d.month}-${d.day}`);
             },
           })}
--- src/components/templates/FormLayout.tsx
+++ src/components/templates/FormLayout.tsx
@@ -7,8 +7,6 @@ import { FlexWrapper } from "../atoms/wrapper/FlexWrapper";
 import { SWrapper } from "../atoms/wrapper/Wrapper";
 import styled from "styled-components";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { validateState } from "../../store/validateState";
-import { useRecoilState } from "recoil";
 import { useButton } from "../../hooks/useButton";
 
 type Props = {
@@ -22,19 +20,24 @@ type Select = {
 };
 
 export const FormLayout: VFC<Props> = (props) => {
-  const [isFailure, setIsFailure] = useRecoilState(validateState);
   const { onClickNext } = useButton();
   const { PrimaryContent, SecondaryContent } = props;
   console.log("FormLayoutコンポーネント");
   const methods = useForm<Select>();
-  console.log(methods.formState.errors);
+  console.log(methods.formState.errors.theme);
   const onSubmit: SubmitHandler<Select> = (data) => {
     console.log(data);
     console.log(methods.formState.errors);
-    if (methods.formState.errors.theme) {
-      setIsFailure({ isFailure: true });
-    } else {
+    if (
+      !methods.formState.errors.theme &&
+      /http:\/\/localhost:3000\/$/.test(location.href)
+    ) {
       onClickNext("/period");
+    } else if (
+      !methods.formState.errors.period &&
+      /\/period$/.test(location.href)
+    ) {
+      onClickNext("/confirm");
     }
   };
   return (
@@ -47,6 +50,9 @@ export const FormLayout: VFC<Props> = (props) => {
                 {PrimaryContent}
                 {methods.formState.errors.theme && (
                   <SErrorMessage>*必須項目です。</SErrorMessage>
+                )}
+                {methods.formState.errors.period && (
+                  <SErrorMessage>*過去の日付は選択できません。</SErrorMessage>
                 )}
               </ExtendWrapper>
             </FlexWrapper>
