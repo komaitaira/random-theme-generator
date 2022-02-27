4e3f4bc8ad86653169479ff9ee1ac6cc716964b7
komaitaira <komaitaira@gmail.com>
2022-02-27 15:39:21 +0900

    Fix generateした後のnavigate修正

--- src/hooks/useButton.ts
+++ src/hooks/useButton.ts
@@ -34,7 +34,7 @@ export const useButton = () => {
       const res = await axios.get(`http://localhost:8000${path}`);
       setThemeList(res.data.themelist);
       console.log("onClickGenerate called.");
-      navigate("/default/result");
+      navigate("/result");
     } catch (error) {
       console.error(error);
     } finally {
