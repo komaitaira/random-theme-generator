fd5a12d623909acbb9c35b23d7d65e4c37a7baf8
komaitaira <komaitaira@gmail.com>
2022-03-20 23:20:32 +0900

    Fix env修正、main.py処理修正

--- api/main.py
+++ api/main.py
@@ -14,13 +14,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-db = os.environ.get('POSTGRES_DB')
-user = os.environ.get('POSTGRES_USER')
-password = os.environ.get('POSTGRES_PASSWORD')
+def get_connection():
+    if os.environ.get('IS_PRODUCTION'):
+        host = os.environ.get('PROD_POSTGRES_HOST')
+        port = os.environ.get('PROD_POSTGRES_PORT')
+        db = os.environ.get('PROD_POSTGRES_DB')
+        user = os.environ.get('PROD_POSTGRES_USER')
+        password = os.environ.get('PROD_POSTGRES_PASSWORD')
+        return psycopg2.connect(f"host={host} port={port} dbname={db} user={user} password={password}")
+    else:
+        # host = os.environ.get('POSTGRES_HOST')
+        # port = os.environ.get('POSTGRES_PORT')
+        db = os.environ.get('POSTGRES_DB')
+        user = os.environ.get('POSTGRES_USER')
+        password = os.environ.get('POSTGRES_PASSWORD')
+        return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
 
 @app.get("/generate")
 async def generate(limit: int = 10):
-    connection = psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
+    connection = get_connection()
     cur = connection.cursor()
     cur.execute(f"select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT {limit};")
     wordlist = [ record[2] for record in cur.fetchall()]
