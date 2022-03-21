5f568d22c5aecc41b44844c2eb8aef4af493daaa
komaitaira <komaitaira@gmail.com>
2022-03-21 14:42:00 +0900

    Fix レスポンステスト2

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
-import psycopg2
+import sqlite3
 
 app = FastAPI()
 origins = ["http://localhost:3000", "https://random-theme-generator.vercel.app"]
@@ -20,15 +19,12 @@ def get_connection():
     password = os.environ['POSTGRES_PASSWORD']
     return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
 
+conn = sqlite3.connect("./../wnjpn.db")
+
 @app.get("/generate")
 async def generate(limit: int = 10):
-    print(os.environ.get('IS_PRODUCTION'))
-    return {"themelist": [
-    "テスト",
-    "ナキハクチョウ",
-    "レイシ",
-    "天下太平",
-    "勢力がなくなる"
-    ]}
+    cur = conn.execute(f"select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT {limit};")
+    wordlist = [ record[2] for record in cur.fetchall()]
+    return {"themelist": wordlist}
 
 
