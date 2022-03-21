1d8a7b1b83f928f238733b265afe6e9258badb78
komaitaira <komaitaira@gmail.com>
2022-03-21 15:15:48 +0900

    Fix レスポンステスト4

--- api/main.py
+++ api/main.py
@@ -15,24 +15,22 @@ app.add_middleware(
 )
 
 def get_connection():
-    # if os.environ.get('IS_PRODUCTION'):
-    #     # host = os.environ.get('PROD_POSTGRES_HOST')
-    #     # port = os.environ.get('PROD_POSTGRES_PORT')
-    #     # db = os.environ.get('PROD_POSTGRES_DB')
-    #     # user = os.environ.get('PROD_POSTGRES_USER')
-    #     # password = os.environ.get('PROD_POSTGRES_PASSWORD')
-    #     DATABASE_URL = os.environ['DATABASE_URL']
-    #     return psycopg2.connect(DATABASE_URL, sslmode='require')
-    #     # return psycopg2.connect(f"host={host} port={port} dbname={db} user={user} password={password}")
-    # else:
+    if os.environ.get('IS_PRODUCTION'):
+        # host = os.environ.get('PROD_POSTGRES_HOST')
+        # port = os.environ.get('PROD_POSTGRES_PORT')
+        # db = os.environ.get('PROD_POSTGRES_DB')
+        # user = os.environ.get('PROD_POSTGRES_USER')
+        # password = os.environ.get('PROD_POSTGRES_PASSWORD')
+        DATABASE_URL = os.environ['DATABASE_URL']
+        return psycopg2.connect(DATABASE_URL, sslmode='require')
+        # return psycopg2.connect(f"host={host} port={port} dbname={db} user={user} password={password}")
+    else:
         # host = os.environ.get('POSTGRES_HOST')
         # port = os.environ.get('POSTGRES_PORT')
-        # db = os.environ['POSTGRES_DB']
-        # user = os.environ['POSTGRES_USER']
-        # password = os.environ['POSTGRES_PASSWORD']
-        # return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
-        DATABASE_URL = "postgres://omxfykfrhsxteo:161b96dcb80df2cbcac77518704fc24a3516f40a992cdd3876b9e820ab237004@ec2-52-71-217-158.compute-1.amazonaws.com:5432/d6dhdbhik40m83"
-        return psycopg2.connect(DATABASE_URL, sslmode='require')
+        db = os.environ['POSTGRES_DB']
+        user = os.environ['POSTGRES_USER']
+        password = os.environ['POSTGRES_PASSWORD']
+        return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
 
 @app.get("/generate")
 async def generate(limit: int = 10):
