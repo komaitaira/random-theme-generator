606ef480d4b623afd5e803571f135c1d07c46bbd
komaitaira <komaitaira@gmail.com>
2022-03-21 14:42:00 +0900

    Fix レスポンステスト2

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
-import psycopg2
+import sqlite3
 
 app = FastAPI()
 origins = ["http://localhost:3000", "https://random-theme-generator.vercel.app"]
@@ -13,40 +12,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-def get_connection():
-    if os.environ.get('IS_PRODUCTION'):
-        # host = os.environ.get('PROD_POSTGRES_HOST')
-        # port = os.environ.get('PROD_POSTGRES_PORT')
-        # db = os.environ.get('PROD_POSTGRES_DB')
-        # user = os.environ.get('PROD_POSTGRES_USER')
-        # password = os.environ.get('PROD_POSTGRES_PASSWORD')
-        DATABASE_URL = os.environ['DATABASE_URL']
-        return psycopg2.connect(DATABASE_URL, sslmode='require')
-        # return psycopg2.connect(f"host={host} port={port} dbname={db} user={user} password={password}")
-    else:
-        # host = os.environ.get('POSTGRES_HOST')
-        # port = os.environ.get('POSTGRES_PORT')
-        db = os.environ['POSTGRES_DB']
-        user = os.environ['POSTGRES_USER']
-        password = os.environ['POSTGRES_PASSWORD']
-        return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
+conn = sqlite3.connect("./../wnjpn.db")
 
 @app.get("/generate")
 async def generate(limit: int = 10):
-    print(os.environ.get('IS_PRODUCTION'))
-    # connection = get_connection()
-    # cur = connection.cursor()
-    # cur.execute(f"select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT {limit};")
-    # wordlist = [ record[2] for record in cur.fetchall()]
-    # cur.close() 
-    # connection.close()
-    return {"themelist": [
-    "テスト",
-    "ナキハクチョウ",
-    "レイシ",
-    "天下太平",
-    "勢力がなくなる"
-    ]}
+    cur = conn.execute(f"select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT {limit};")
+    wordlist = [ record[2] for record in cur.fetchall()]
+    return {"themelist": wordlist}
 
 
