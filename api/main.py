f4fc045ce4453ce8ae4fc439606bc70248969dfd
komaitaira <komaitaira@gmail.com>
2022-03-23 00:34:21 +0900

    Fix main.py

--- api/main.py
+++ api/main.py
@@ -22,6 +22,22 @@ def get_connection():
     return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
 
 conn = sqlite3.connect("./../wnjpn.db")
+    if os.environ.get('IS_PRODUCTION'):
+        # host = os.environ.get('PROD_POSTGRES_HOST')
+        # port = os.environ.get('PROD_POSTGRES_PORT')
+        # db = os.environ.get('PROD_POSTGRES_DB')
+        # user = os.environ.get('PROD_POSTGRES_USER')
+        # password = os.environ.get('PROD_POSTGRES_PASSWORD')
+        DATABASE_URL = os.environ['DATABASE_URL']
+        return psycopg2.connect(DATABASE_URL, sslmode='require')
+        # return psycopg2.connect(f"host={host} port={port} dbname={db} user={user} password={password}")
+    else:
+        # host = os.environ.get('POSTGRES_HOST')
+        # port = os.environ.get('POSTGRES_PORT')
+        db = os.environ['POSTGRES_DB']
+        user = os.environ['POSTGRES_USER']
+        password = os.environ['POSTGRES_PASSWORD']
+        return psycopg2.connect(f"host=db port=5432 dbname={db} user={user} password={password}")
 
 @app.get("/generate")
 async def generate(limit: int = 10):
